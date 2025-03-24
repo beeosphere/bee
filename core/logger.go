ae9a2d0a87b7dcf390bbc3cbea8d1e149be101bc
Daniel <dani@kraaisoft.com>
2025-03-24 19:02:26 +0100

    Global lock and new logger

--- core/logger.go
+++ core/logger.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -20,59 +22,83 @@ type Logger interface {
 
 // logrusLogger is a wrapper around logrus.Logger that implements the Logger interface.
 type logrusLogger struct {
-	logger *logrus.Logger
+	logger  *logrus.Logger
+	agent   string
+	channel string
+	prefix  string
+}
+
+func (l *logrusLogger) SetPrefix(agent, channel string) {
+	l.agent = agent
+	l.channel = channel
+	if channel == "" {
+		l.prefix = fmt.Sprintf("[%s] ", agent)
+	} else {
+		l.prefix = fmt.Sprintf("[%s/%s] ", agent, channel)
+	}
+}
+
+func (l *logrusLogger) SetLevel(level logrus.Level) {
+	l.logger.SetLevel(level)
 }
 
 // newLogrusLogger creates a new LogrusLogger.
 func newLogrusLogger() *logrusLogger {
-	return &logrusLogger{logger: logrus.StandardLogger()}
+	return &logrusLogger{
+		logger: logrus.StandardLogger(),
+	}
 }
 
 // Debug logs a debug message.
 func (l *logrusLogger) Trace(args ...interface{}) {
+	args = append([]interface{}{l.prefix}, args...)
 	l.logger.Trace(args...)
 }
 
 // Debugf logs a debug message using a format string.
 func (l *logrusLogger) Tracef(format string, args ...interface{}) {
-	l.logger.Tracef(format, args...)
+	l.logger.Tracef(l.prefix+format, args...)
 }
 
 // Debug logs a debug message.
 func (l *logrusLogger) Debug(args ...interface{}) {
+	args = append([]interface{}{l.prefix}, args...)
 	l.logger.Debug(args...)
 }
 
 // Debugf logs a debug message using a format string.
 func (l *logrusLogger) Debugf(format string, args ...interface{}) {
-	l.logger.Debugf(format, args...)
+	l.logger.Debugf(l.prefix+format, args...)
 }
 
 // Info logs an info message.
 func (l *logrusLogger) Info(args ...interface{}) {
+	args = append([]interface{}{l.prefix}, args...)
 	l.logger.Info(args...)
 }
 
 // Infof logs an info message using a format string.
 func (l *logrusLogger) Infof(format string, args ...interface{}) {
-	l.logger.Infof(format, args...)
+	l.logger.Infof(l.prefix+format, args...)
 }
 
 // Warn logs a warning message.
 func (l *logrusLogger) Warn(args ...interface{}) {
+	args = append([]interface{}{l.prefix}, args...)
 	l.logger.Warn(args...)
 }
 
 func (l *logrusLogger) Warnf(format string, args ...interface{}) {
-	l.logger.Warnf(format, args...)
+	l.logger.Warnf(l.prefix+format, args...)
 }
 
 // Error logs an error message.
 func (l *logrusLogger) Error(args ...interface{}) {
+	args = append([]interface{}{l.prefix}, args...)
 	l.logger.Error(args...)
 }
 
 // Errorf logs an error message using a format string.
 func (l *logrusLogger) Errorf(format string, args ...interface{}) {
-	l.logger.Errorf(format, args...)
+	l.logger.Errorf(l.prefix+format, args...)
 }
